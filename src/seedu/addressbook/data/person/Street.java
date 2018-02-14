fa4e8f8f94c260942ca208f886a46ea448c88ee7
ISAAACALAPTOP\Isaac Lau <isaaaca@gmail.com>
2018-02-14 20:07:48 +0800

    Added Street Class

---
+++ src/seedu/addressbook/data/person/Street.java
@@ -0,0 +1,40 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents a Person's Street in the address book.
+ */
+public class Street {
+
+    public static final String EXAMPLE = "Something Ave 3";
+
+    public final String value;
+    private boolean isPrivate;
+
+    public Street(String street, boolean isPrivate) throws IllegalValueException {
+        this.isPrivate = isPrivate;
+        this.value = street.trim();
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Street // instanceof handles nulls
+                && this.value.equals(((Street) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
