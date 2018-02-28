1ab22ff5fd8d0fbc73c0b0c0dabcd82a7d49b23f
ISAAACALAPTOP\Isaac Lau <isaaaca@gmail.com>
2018-02-28 17:52:20 +0800

    Abstracted duplicate methods of Address, Email, and Phone Classes into an abstract parent class Contact

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,34 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Abstract parent class for different contact classes
+ * Inherited by
+ * @see Email
+ * @see Address
+ * @see Phone
+ */
+public abstract class Contact {
+    public String value;
+    protected boolean isPrivate;
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
